946d3381ce0ad21425657fc0eac8f149fa5ccb34
Pat York <guitarp_11@yahoo.com>
2014-03-06 11:05:57 -0800

    Single Node cluster bash for CS447

--- make-single-node.sh
+++ make-single-node.sh
@@ -1,80 +1,92 @@
-cd ~ 
+## Set up a single Node Hadoop cluster
+## Forked and modified by Pat York from https://github.com/ericduq/hadoop-scripts
+## Suitable for Ubuntu and Debian distros
+
+#cd ~ 
 #sudo apt-get update
 
-# Download java jdk
+## Download java jdk
 #sudo apt-get install openjdk-7-jdk
 #cd /usr/lib/jvm
 #sudo ln -s java-7-openjdk-amd64 jdk
 
-# Uncommment to install ssh 
+## install ssh 
 #sudo apt-get install openssh-server
 
-# Add hadoop user
+## Add hadoop user
 #sudo addgroup hadoop
 #sudo adduser --ingroup hadoop hduser
 #sudo adduser hduser sudo
 
-# Generate keys
+## Generate keys
 #sudo -u hduser ssh-keygen -t rsa -P ''
 #sudo sh -c 'cat /home/hduser/.ssh/id_rsa.pub >> /home/hduser/.ssh/authorized_keys'
 #ssh localhost
 
-# Download Hadoop and set permissons
-cd ~
-if [ ! -f hadoop-2.2.0.tar.gz ]; then
-	wget http://www.webhostingjams.com/mirror/apache/hadoop/common/hadoop-2.2.0/hadoop-2.2.0.tar.gz
-fi
-sudo tar vxzf hadoop-2.2.0.tar.gz -C /usr/local
-cd /usr/local
-sudo mv hadoop-2.2.0 hadoop
-sudo chown -R hduser:hadoop hadoop
+## Download Hadoop and set permissons
+#cd ~
+#if [ ! -f hadoop-2.2.0.tar.gz ]; then
+#	wget http://www.webhostingjams.com/mirror/apache/hadoop/common/hadoop-2.2.0/hadoop-2.2.0.tar.gz
+#fi
+#sudo tar vxzf hadoop-2.2.0.tar.gz -C /usr/local
+#cd /usr/local
+#sudo mv hadoop-2.2.0 hadoop
+#sudo chown -R hduser:hadoop hadoop
 
-# Hadoop variables
-sudo sh -c 'echo export JAVA_HOME=/usr/lib/jvm/jdk/ >> /home/hduser/.bashrc'
-sudo sh -c 'echo export HADOOP_INSTALL=/usr/local/hadoop >> /home/hduser/.bashrc'
-sudo sh -c 'echo export PATH=\$PATH:\$HADOOP_INSTALL/bin >> /home/hduser/.bashrc'
-sudo sh -c 'echo export PATH=\$PATH:\$HADOOP_INSTALL/sbin >> /home/hduser/.bashrc'
-sudo sh -c 'echo export HADOOP_MAPRED_HOME=\$HADOOP_INSTALL >> /home/hduser/.bashrc'
-sudo sh -c 'echo export HADOOP_COMMON_HOME=\$HADOOP_INSTALL >> /home/hduser/.bashrc'
-sudo sh -c 'echo export HADOOP_HDFS_HOME=\$HADOOP_INSTALL >> /home/hduser/.bashrc'
-sudo sh -c 'echo export YARN_HOME=\$HADOOP_INSTALL >> /home/hduser/.bashrc'
-sudo sh -c 'echo export HADOOP_COMMON_LIB_NATIVE_DIR=\$\{HADOOP_INSTALL\}/lib/native >> /home/hduser/.bashrc'
-sudo sh -c 'echo export HADOOP_OPTS=\"-Djava.library.path=\$HADOOP_INSTALL/lib\" >> /home/hduser/.bashrc'
+## Hadoop variables
+#sudo sh -c 'echo export JAVA_HOME=/usr/lib/jvm/jdk/ >> /home/hduser/.bashrc'
+#sudo sh -c 'echo export HADOOP_INSTALL=/usr/local/hadoop >> /home/hduser/.bashrc'
+#sudo sh -c 'echo export PATH=\$PATH:\$HADOOP_INSTALL/bin >> /home/hduser/.bashrc'
+#sudo sh -c 'echo export PATH=\$PATH:\$HADOOP_INSTALL/sbin >> /home/hduser/.bashrc'
+#sudo sh -c 'echo export HADOOP_MAPRED_HOME=\$HADOOP_INSTALL >> /home/hduser/.bashrc'
+#sudo sh -c 'echo export HADOOP_COMMON_HOME=\$HADOOP_INSTALL >> /home/hduser/.bashrc'
+#sudo sh -c 'echo export HADOOP_HDFS_HOME=\$HADOOP_INSTALL >> /home/hduser/.bashrc'
+#sudo sh -c 'echo export YARN_HOME=\$HADOOP_INSTALL >> /home/hduser/.bashrc'
+#sudo sh -c 'echo export HADOOP_COMMON_LIB_NATIVE_DIR=\$\{HADOOP_INSTALL\}/lib/native >> /home/hduser/.bashrc'
+#sudo sh -c 'echo export HADOOP_OPTS=\"-Djava.library.path=\$HADOOP_INSTALL/lib\" >> /home/hduser/.bashrc'
 
-# Modify JAVA_HOME 
-cd /usr/local/hadoop/etc/hadoop
-sudo -u hduser sed -i.bak s=\${JAVA_HOME}=/usr/lib/jvm/jdk/=g hadoop-env.sh
-pwd
+## Modify JAVA_HOME 
+#cd /usr/local/hadoop/etc/hadoop
+#sudo -u hduser sed -i.bak s=\${JAVA_HOME}=/usr/lib/jvm/jdk/=g hadoop-env.sh
+#pwd
 
-# Check that Hadoop is installed
-/usr/local/hadoop/bin/hadoop version
+## Check that Hadoop is installed
+#/usr/local/hadoop/bin/hadoop version
 
-# Edit configuration files
-sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>fs\.default\.name\</name>\<value>hdfs://localhost:9000\</value>\</property>=g' core-site.xml 
-sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>yarn\.nodemanager\.aux-services</name>\<value>mapreduce_shuffle</value>\</property>\<property>\<name>yarn.nodemanager.aux-services.mapreduce.shuffle.class</name>\<value>org\.apache\.hadoop\.mapred\.ShuffleHandler</value>\</property>=g' yarn-site.xml
+## Edit configuration files
+#sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>fs\.default\.name\</name>\<value>hdfs://localhost:9000\</value>\</property>=g' core-site.xml 
+#sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>yarn\.nodemanager\.aux-services</name>\<value>mapreduce_shuffle</value>\</property>\<property>\<name>yarn.nodemanager.aux-services.mapreduce.shuffle.class</name>\<value>org\.apache\.hadoop\.mapred\.ShuffleHandler</value>\</property>=g' yarn-site.xml
   
-sudo -u hduser cp mapred-site.xml.template mapred-site.xml
-sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>mapreduce\.framework\.name</name>\<value>yarn</value>\</property>=g' mapred-site.xml
+#sudo -u hduser cp mapred-site.xml.template mapred-site.xml
+#sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>mapreduce\.framework\.name</name>\<value>yarn</value>\</property>=g' mapred-site.xml
  
-cd ~
-sudo mkdir -p mydata/hdfs/namenode
-sudo mkdir -p mydata/hdfs/datanode
+#cd ~
+#sudo mkdir -p mydata/hdfs/namenode
+#sudo mkdir -p mydata/hdfs/datanode
 
-cd /usr/local/hadoop/etc/hadoop
-sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>dfs\.replication</name>\<value>1\</value>\</property>\<property>\<name>dfs\.namenode\.name\.dir</name>\<value>file:/home/hduser/mydata/hdfs/namenode</value>\</property>\<property>\<name>dfs\.datanode\.data\.dir</name>\<value>file:/home/hduser/mydata/hdfs/datanode</value>\</property>=g' hdfs-site.xml
+#cd /usr/local/hadoop/etc/hadoop
+#sudo -u hduser sed -i.bak 's=<configuration>=<configuration>\<property>\<name>dfs\.replication</name>\<value>1\</value>\</property>\<property>\<name>dfs\.namenode\.name\.dir</name>\<value>file:/home/hduser/mydata/hdfs/namenode</value>\</property>\<property>\<name>dfs\.datanode\.data\.dir</name>\<value>file:/home/hduser/mydata/hdfs/datanode</value>\</property>=g' hdfs-site.xml
 
 
-# Format Namenode
+## Format Namenode
 #sudo sh -c 'hdfs namenode -format'
 
-# Start Hadoop Service
+## Start Hadoop Service
 #sudo -u hduser start-dfs.sh
 #sudo -u hduser start-yarn.sh
 
-# Check status
+## Check status
 #sudo -u hduser jps
 
-# Example
+## Example
 # sudo -u hduser cd /usr/local/hadoop
 # sudo -u hduser hadoop jar ./share/hadoop/mapreduce/hadoop-mapreduce-examples-2.2.0.jar pi 2 5
+
+## Wasn't very close to te correct answer, was it? Lets increase the number of maps and see if we can get closer
+## (uncomment/run these lines one at a time
+# sudo -u hduser hadoop jar ./share/hadoop/mapreduce/hadoop-mapreduce-examples-2.2.0.jar pi 4 10
+
+# sudo -u hduser hadoop jar ./share/hadoop/mapreduce/hadoop-mapreduce-examples-2.2.0.jar pi 8 20
+
+# sudo -u hduser hadoop jar ./share/hadoop/mapreduce/hadoop-mapreduce-examples-2.2.0.jar pi 16 40
 
